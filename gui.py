36addb762418a2678d0652ea86c42d00b5a2e25a
Marcin <marcin.karbowniczyn@gmail.com>
2023-10-19 14:34:53 +0200

    Add some comments into the gui file

--- gui.py
+++ gui.py
@@ -1,8 +1,13 @@
 import functions
 import PySimpleGUI as sg
 
+# Ta metoda tworzy text instances/label instances
 label = sg.Text('Type in a to-do:')
+
+# Ta metoda tworzy input space
 input_box = sg.InputText('Enter a todo')
+
+# Ta metoda tworzy przyciski
 add_button = sg.Button('Add')
 
 # Layout -> Every single list element is a seperate rown, so if I wanted to put one row below another it would be like this: [[label][input_box]]
