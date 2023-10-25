2b6fdaa6b3d1c6e8544cdc1fc21f45deb5a46b2f
Marcin <marcin.karbowniczyn@gmail.com>
2023-10-25 13:51:22 +0200

    Implement complete and exit buttons

--- gui.py
+++ gui.py
@@ -3,11 +3,11 @@ import PySimpleGUI as sg
 
 # Ta metoda tworzy text instances/label instances
 label = sg.Text('Type in a to-do:')
-# Ta metoda tworzy input space
 input_box = sg.InputText(tooltip='Enter a todo', key='todo_input_box')
-# Ta metoda tworzy przyciski
 add_button = sg.Button('Add')
 edit_button = sg.Button('Edit')
+complete_button = sg.Button('Complete')
+exit_button = sg.Button('Exit')
 
 list_box = sg.Listbox(
     values=functions.get_todos(),
@@ -17,14 +17,16 @@ list_box = sg.Listbox(
 
 # Layout -> Every single list element is a seperate rown, so if I wanted to put one row below another it would be like this: [[label][input_box]]. The third argument is a font and its size.
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Helvetica', 15))
 
 # Method read() displays a window on the screen. It also produces the event object, which contains the name of a button that was clicked, and also contents of input fields.
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(event, values)
     match event:
         case 'Add':
             todos = functions.get_todos()
@@ -33,11 +35,9 @@ while True:
 
             window['todosList'].update(values=todos)
             window['todo_input_box'].update(value='')
-            continue
 
         case 'todosList':
             window['todo_input_box'].update(value=values['todosList'][0].replace('\n', ''))
-            continue
 
         case 'Edit':
             todo_to_edit = values['todosList'][0]
@@ -50,7 +50,22 @@ while True:
             # Wszystkie elementy są zapisane na obiekcie window i w taki sposób mamy do nich dostęp
             window['todosList'].update(values=todos)
             window['todo_input_box'].update(value='')
-            continue
+
+        case 'Complete':
+            todo_to_complete = values['todosList'][0]
+            todos = functions.get_todos()
+
+            # Dwa sposoby na usunięcie elementu z listy
+            # todos.pop(todos.index(todo_to_complete))
+            todos.remove(todo_to_complete)
+
+            functions.write_todos(todos)
+
+            window['todosList'].update(values=todos)
+            window['todo_input_box'].update(value='')
+
+        case 'Exit':
+            break
 
         case sg.WIN_CLOSED:
             break
