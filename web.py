cf6a8b75c0dd226c4edfc3c3d0e406fd9e3da5e1
Marcin <marcin.karbowniczyn@gmail.com>
2023-10-27 12:58:01 +0200

    Create a submit form, implement a remove TODO functionality, prepare for deployment

--- web.py
+++ web.py
@@ -1,15 +1,48 @@
 import streamlit as st
 import functions
+import os
+import random
+
+print('            a     aa      a              '.strip())
+
+if not os.path.exists('todos.txt'):
+    with open('todos.txt', 'w') as file:
+        pass
 
 todos = functions.get_todos()
 
+
+def add_todo(user_input_local):
+    todo_local = user_input_local.strip() + '\n'
+    todos.append(todo_local)
+    functions.write_todos(todos)
+
+
 st.title('My Todo App')
-# st.subheader('This is my todo app.')
-# st.write('This app serves you to increase your productivity!!!!')
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=f'checkbox{index}')
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[f'checkbox{index}']
+        st.rerun()
 
-st.text_input(label='Enter a todo:',
-              placeholder='Enter a new todo...',
-              key='todo_input')
+with st.form("my_form", clear_on_submit=True):
+    user_input = st.text_input(label='Enter a todo that is at least 3 and maximally 30 characters long:',
+                               placeholder='Enter a new todo...',
+                               key='todo_input')
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        if 3 <= len(user_input.replace(' ', '')) <= 30:
+            add_todo(user_input)
+            st.rerun()
+        else:
+            st.error('A todo must contain minimum of 3 and maximum of 30 characters.')
+
+# Check when exactly code app is being refreshed
+# st.session_state['random'] = random.randint(1, 100)
+
+# st.session_state
